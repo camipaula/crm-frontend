573baccbddb0821b84dd0a3202a188e145c9c9a2
camipaula <103073699+camipaula@users.noreply.github.com>
2025-03-27 21:27:33 -0500

    Update CalendarioVendedora.jsx

--- src/pages/CalendarioVendedora.jsx
+++ src/pages/CalendarioVendedora.jsx
@@ -58,17 +58,6 @@ const CalendarioVendedora = () => {
     const minutos = String(fecha.getMinutes()).padStart(2, '0');
     return `${año}-${mes}-${dia}T${horas}:${minutos}`;
   };
-
-  const convertirUTCaLocalDatetime = (fechaUTC) => {
-    const fecha = new Date(fechaUTC);
-    const año = fecha.getFullYear();
-    const mes = String(fecha.getMonth() + 1).padStart(2, "0");
-    const dia = String(fecha.getDate()).padStart(2, "0");
-    const horas = String(fecha.getHours()).padStart(2, "0");
-    const minutos = String(fecha.getMinutes()).padStart(2, "0");
-    return `${año}-${mes}-${dia}T${horas}:${minutos}`;
-  };
-  
   
 
   const cargarAgenda = async () => {
